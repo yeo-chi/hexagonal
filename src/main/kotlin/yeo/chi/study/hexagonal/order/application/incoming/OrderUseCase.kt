579a03e816abc44e30d712a455cbda5c094fcab4
yeo-chi <00www.black@gmail.com>
2023-09-06 17:55:54 +0900

    useCase 작성

---
+++ src/main/kotlin/yeo/chi/study/hexagonal/order/application/incoming/OrderUseCase.kt
@@ -0,0 +1,15 @@
+package yeo.chi.study.hexagonal.order.application.incoming
+
+import yeo.chi.study.hexagonal.order.domain.Order
+import yeo.chi.study.hexagonal.order.domain.vo.CreateOrder
+import yeo.chi.study.hexagonal.order.domain.vo.SearchCondition
+
+interface OrderUseCase {
+    fun getList(searchCondition: SearchCondition): List<Order>
+
+    fun getOne(id: Long): Order
+
+    fun create(createOrder: CreateOrder): Order
+
+    fun delete(id: Long)
+}
