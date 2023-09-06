2d7da7a9983ae351569873044fa729562cf8aa1b
yeo-chi <00www.black@gmail.com>
2023-09-06 15:32:02 +0900

    init

---
+++ src/main/kotlin/yeo/chi/study/hexagonal/HexagonalApplication.kt
@@ -0,0 +1,11 @@
+package yeo.chi.study.hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexagonalApplication>(*args)
+}
