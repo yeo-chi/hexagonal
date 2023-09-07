233c0d236226eb5f9b8c137bde6ed9e706f9f691
yeo-chi <00www.black@gmail.com>
2023-09-07 15:42:20 +0900

    Order Enum에서 pageable을 가져 올 수 있도록 수정

--- src/main/kotlin/yeo/chi/study/hexagonal/order/adapter/web/v1/controller/data/Order.kt
+++ src/main/kotlin/yeo/chi/study/hexagonal/order/adapter/web/v1/controller/data/Order.kt
@@ -1,6 +1,8 @@
 package yeo.chi.study.hexagonal.order.adapter.web.v1.controller.data
 
-enum class Order {
-    ASC,
-    DESC,
+import org.springframework.data.domain.Sort
+
+enum class Order(val direction: Sort.Direction) {
+    ASC(Sort.Direction.ASC),
+    DESC(Sort.Direction.DESC),
 }
--- src/main/kotlin/yeo/chi/study/hexagonal/order/domain/vo/SearchCondition.kt
+++ src/main/kotlin/yeo/chi/study/hexagonal/order/domain/vo/SearchCondition.kt
@@ -1,11 +1,21 @@
 package yeo.chi.study.hexagonal.order.domain.vo
 
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import yeo.chi.study.hexagonal.order.adapter.web.v1.controller.data.Order
 
 class SearchCondition(
-    val order: Order,
+    order: Order,
 
-    val limit: Long,
+    limit: Long,
 
-    val offset: Long,
-)
+    offset: Long,
+) {
+    val pageable: Pageable
+
+    init {
+        pageable = PageRequest.of(
+            offset.toInt(), limit.toInt(), order.direction
+        )
+    }
+}
