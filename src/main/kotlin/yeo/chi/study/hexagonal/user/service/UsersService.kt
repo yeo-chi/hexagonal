6877e5fbf3c124bccb5219ec5915bbd7d033a943
yeo-chi <00www.black@gmail.com>
2023-10-12 16:06:18 +0900

    transactional 주석 추가

--- src/main/kotlin/yeo/chi/study/hexagonal/user/service/UsersService.kt
+++ src/main/kotlin/yeo/chi/study/hexagonal/user/service/UsersService.kt
@@ -2,6 +2,7 @@ package yeo.chi.study.hexagonal.user.service
 
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import yeo.chi.study.hexagonal.user.controller.data.SignInRequest
 import yeo.chi.study.hexagonal.user.controller.data.SignUpRequest
 import yeo.chi.study.hexagonal.user.persistant.entity.UserEntity
@@ -12,12 +13,14 @@ class UsersService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
 ) {
+    @Transactional(readOnly = true)
     fun validUser(signInRequest: SignInRequest) {
         userRepository.findByUserId(userId = signInRequest.userId)?.also {
             check(passwordEncoder.matches(signInRequest.password, it.password))
         } ?: throw NoSuchElementException()
     }
 
+    @Transactional
     fun createUser(signUpRequest: SignUpRequest) {
         userRepository.findByUserId(userId = signUpRequest.userId) ?: apply {
             UserEntity.create(signUpRequest = signUpRequest, encoder = passwordEncoder).also {
