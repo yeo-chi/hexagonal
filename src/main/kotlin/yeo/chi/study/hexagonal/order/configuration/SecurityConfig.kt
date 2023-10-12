c34d620b43f11d639b0dffebc750d4a30315506f
yeo-chi <00www.black@gmail.com>
2023-10-12 13:54:36 +0900

    security 설정

---
+++ src/main/kotlin/yeo/chi/study/hexagonal/order/configuration/SecurityConfig.kt
@@ -0,0 +1,29 @@
+package yeo.chi.study.hexagonal.order.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity) = http
+        .csrf { it.disable() }
+        .headers {
+            it.frameOptions { frameOptions ->
+                frameOptions.sameOrigin()
+            }
+        }
+        .authorizeHttpRequests {
+            it.requestMatchers(
+                "/",
+                "/swagger-ui/**",
+                "/v3/**",
+            ).permitAll()
+                .anyRequest()
+                .authenticated()
+        }
+        .sessionManagement { it.sessionCreationPolicy(STATELESS) }
+        .build()
+}
